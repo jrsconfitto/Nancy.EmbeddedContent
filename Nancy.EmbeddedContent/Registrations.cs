04a92c64e580cd07d612a0bbe725b9a1fc5a0287
James R Sconfitto <james@exele.com>
2014-06-06 14:26:44 -0400

    ApplicationRegistrations => Registrations
    Re: https://github.com/NancyFx/Nancy/pull/1503

--- Nancy.EmbeddedContent/Registrations.cs
+++ Nancy.EmbeddedContent/Registrations.cs
@@ -6,17 +6,18 @@
     /// <summary>
     /// Application registrations supporting views embedded in an assembly
     /// </summary>
-    public class Registrations : ApplicationRegistrations
+    public class EmbeddedRegistrations : Registrations
     {
         /// <summary>
         /// Registers the <see cref="EmbeddedViewLocationProvider"/> with the container to enable
         /// embedded views
         /// </summary>
-        public Registrations()
+        public EmbeddedRegistrations()
         {
             this.Register<IResourceAssemblyProvider>(typeof(ResourceAssemblyProvider));
             this.Register<IResourceReader>(typeof(DefaultResourceReader));
             this.Register<IViewLocationProvider>(typeof(EmbeddedViewLocationProvider));
         }
+
     }
 }
